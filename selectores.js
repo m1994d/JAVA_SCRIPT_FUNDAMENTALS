58bffdd6c4bfae51275619e4af6876a44933242e
m1994d <marlondiaz2994@gmail.com>
2022-09-09 11:45:51 -0500

    fixing code HTML and JS

---
+++ selectores.js
@@ -0,0 +1,20 @@
+// SELECTORES INDIVIDUALES
+//getElementById(); SELECCIONA EL ELEMENTO POR UN ATRIBUTO ID
+//querySelector(); SELECCIONA EL ELEMENTO CON UN SELECTOR CSS, DEVOLVIENDO EL PRIMER ELEMENTO QUE COINCIDA
+
+//SELECTORES MULTIPLES
+//getElementsByClassName(); SELECCIONA TODOS LOS ELEMENTOS QUE TENGAN LA CLASE ESPECIFICADA
+//getElementsByTagName(); SELECCIONA TODOS LOS ELEMENTOS CON LA ETIQUETA ESPECIFICADA
+//querySelectorAll(); SELECCIONA TODOS LOS ELEMENTOS CON EL SELECTOR CSS ESPECIFICADO
+
+//------------------------------------------------------
+
+// var boton = document.getElementById('boton');
+
+// var segundoParrafo = document.querySelector('#segundo');
+
+// ------------------------------------------------------
+
+// var parrafos = document.getElementsByClassName('texto');
+// var parrafos = document.getElementsByTagName('p');
+// var parrafos = document.querySelectorAll('.texto');
