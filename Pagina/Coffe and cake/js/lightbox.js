de5a9277876e93f6daee2e10b81526361bc057f4
m1994d <marlondiaz2994@gmail.com>
2022-09-26 10:24:00 -0500

    code fix

---
+++ Pagina/Coffe and cake/js/lightbox.js
@@ -0,0 +1,62 @@
+// Objeto con las Propiedades del efecto Lightbox
+var propLightbox = {
+
+	imgContainer: document.getElementsByClassName('lightbox'),
+	imagen: null,
+	imagenSrc: null,
+	cuerpoDom: document.getElementsByTagName('body')[0],
+	lightbox_container: null,
+	modal: null
+
+}
+
+
+// Objeto con los Métodos del efecto Lightbox
+var metLightbox = {
+
+	inicio: function(){
+
+		for (var i = 0; i < propLightbox.imgContainer.length; i++) {
+			propLightbox.imgContainer[i].addEventListener('click', metLightbox.capturaImagen);
+		}
+	},
+
+
+	capturaImagen: function(){
+
+		propLightbox.imagen = this;
+		metLightbox.lightbox(propLightbox.imagen);
+
+	},
+
+
+	lightbox: function(imagen){
+
+		propLightbox.imagenSrc = window.getComputedStyle(imagen, null).backgroundImage.slice(5, -2);
+		
+		propLightbox.cuerpoDom.appendChild(document.createElement('DIV')).setAttribute('id', 'lightbox_container');
+
+		propLightbox.lightbox_container = document.getElementById('lightbox_container');
+
+		propLightbox.lightbox_container.style.width = '100%';
+		propLightbox.lightbox_container.style.height = '100%';
+		propLightbox.lightbox_container.style.position = 'fixed';
+		propLightbox.lightbox_container.style.zIndex = '1000';
+		propLightbox.lightbox_container.style.background = 'rgba(0,0,0,0.8)';
+		propLightbox.lightbox_container.style.top = '0';
+		propLightbox.lightbox_container.style.left = '0';
+
+		propLightbox.lightbox_container.appendChild(document.createElement('DIV')).setAttribute('id', 'modal');
+		propLightbox.modal = document.getElementById('modal');
+
+		propLightbox.modal.style.height = '100%';
+
+		propLightbox.modal.appendChild(document.createElement('IMG')).setAttribute('src', propLightbox.imagenSrc);
+		propLightbox.modal.getElementsByTagName('img')[0].setAttribute('class', 'imagen-modal');
+
+	}
+
+
+}
+
+metLightbox.inicio();
